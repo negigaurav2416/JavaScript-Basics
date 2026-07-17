daf57935276a5f60c14d25ff5d5e94188ac47f08
Gaurav Singh Negi <negigaurav2416@gmail.com>
2026-07-17 17:53:42 +0530

    Add strings.js

---
+++ strings.js
@@ -0,0 +1,29 @@
+let name = "Gaurav negi";
+let des = " will be passout in 2028 ";
+
+//console.log(name + ' ' + sname); //concatination
+//console.log(name.length); //length
+//console.log(`My name is ${name}`); //var
+//console.log(name.toUpperCase()); // uppercase method
+//console.log(name.toLowerCase()); // lowercase
+// console.log(name.trim()); // trim removes the extra spaces
+
+let email = "negigaurav1234@gmail.com";
+
+//console.log(email.includes("@")); // includes tell T/F whether the char is in the given string or not
+//console.log(email.indexOf("g")); // indexOf tells the index of the char in given string
+// startsWith , endsWith
+// console.log(email.slice(0,4)); // slice method
+// console.log(email.slice(-3)); // -ve slicing
+
+console.log(name.replace("negi","singh negi")); // replace
+
+let branches = "CSE, AI/ML, DS, IT";
+
+console.log(branches.split(",")); // split
+
+// Methods in strings does not change the original string...it creates a new string or a copy!!!
+
+
+
+
