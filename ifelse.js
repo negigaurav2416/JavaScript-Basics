fec0e4f5f3a73214452b3fd5a92c2df2769b4cfa
Gaurav Negi <negigaurav2416@gmail.com>
2026-09-11 20:48:13 +0530

    if else practice

---
+++ ifelse.js
@@ -0,0 +1,47 @@
+// Comparison Operators --->    < , > , <= , >= , , = , == , === , != , !== 
+// control flow condition - if , else , else if 
+
+let user = "loggedin"
+if (user = "loggedin"){
+    console.log("The user is logged in!!");
+}
+
+
+let marks = 92;
+if (marks < 50){
+    console.log("D GRADE");
+}
+else if (marks < 60){
+    console.log("C GRADE");
+}
+else if (marks < 70){
+    console.log("C GRADE");
+}
+else if (marks < 80){
+    console.log("B GRADE");
+}
+else if (marks < 90){
+    console.log("A GRADE");
+}
+else{
+    console.log("A+ GRADE");
+}
+
+// AND -> && ..... OR -> ||
+const a = 10;
+const b = 5;
+if (a > 0 && b > 0){
+    console.log("Both are positives"); // both conditions must true to execute this part
+}
+else{
+    console.log("Both are negatives");
+}
+
+const c = -10;
+const d = -5;
+if (c > 0 || d > 0){
+    console.log("Both are positives"); // both conditions must true to execute this part
+}
+else{
+    console.log("Both are not positive");
+}
