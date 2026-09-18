80a1864cbd5859a75bee3047c14fc0470f6195cb
Gaurav Negi <negigaurav2416@gmail.com>
2026-09-19 00:36:23 +0530

    filter method practice

--- loops6.js
+++ loops6.js
@@ -6,6 +6,7 @@
 // console.log(values); // forEach do not return any values 
 
 // Filter() is array method which creates a new array containing only the elements that satisfy a condition
+
 const numbers = [1,2,3,4,5,6,7,8,9,10];
 const even = numbers.filter( (item) => {
     return (item % 2 == 0);
